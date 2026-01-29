617525b5569f354cf245eb9ad4f50dfe51855923
regnernacua <nacua.regner@gmail.com>
2026-01-29 12:05:02 +0800

    Add CORS

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import { sightingsRouter } from './routes/sightings.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 1337;
+
+// CORS (allow Vercel + Local)
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://paranormal-sightings-frontend.vercel.app/'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 app.use(express.json());
 
@@ -23,7 +35,12 @@ mongoose
 // API routes
 app.use('/api/sightings', sightingsRouter);
 
+//Health check
+app.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
